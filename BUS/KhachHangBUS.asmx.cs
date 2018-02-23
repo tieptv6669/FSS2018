f1dfe21737a077955bf2f100e43f0863e37e7393
kemkemkute <kemkem51296@gmail.com>
2018-02-23 16:09:11 +0700

    cho tiep

--- BUS/KhachHangBUS.asmx.cs
+++ BUS/KhachHangBUS.asmx.cs
@@ -22,5 +22,51 @@ namespace BUS
         {
             return "Hello World";
         }
+        [WebMethod]
+        public int KTThongTinThemKH(string soTKLK, DateTime ngayMoTK, string hoTen, DateTime ngaySinh, string ngheNghiep, string soCMTNN, string diaChi, string email, string SDT)
+        {
+            Helper helper = new Helper();
+            if (soTKLK == "")
+            {
+                return 1;
+            }
+            if (hoTen == "")
+            {
+                return 2;
+            }
+            if (ngheNghiep == "")
+            {
+                return 3;
+            }
+            if (soCMTNN == "")
+            {
+                return 4;
+            }
+            if (diaChi == "")
+            {
+                return 5;
+            }
+            if (email == "")
+            {
+                return 6;
+            }
+            if (SDT == "")
+            {
+                return 7;
+            }
+            if (ngayMoTK.Year - ngaySinh.Year < 18)
+            {
+                return 8;
+            }
+            if (helper.ChiChuaChuCai(hoTen) == false)
+            {
+                return 9;
+            }
+            if (helper.ChiChuaChuCai(ngheNghiep) == false)
+            {
+                return 10;
+            }
+            return 0;
+        }
     }
 }
